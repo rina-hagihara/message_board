c5ce8914420fb07d5bed7c6d71efa53baceaf206
rina_ <rina_@DESKTOP-LR1P7G1>
2022-05-18 09:26:17 +0900

    EditServlet.javaを作成」

---
+++ src/controllers/EditServlet.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.protobuf.Message;
+
+import utill.DBUtil;
+
+/**
+ * Servlet implementation class EditServlet
+ */
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntitymanager();
+
+        Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("message", m);
+        request.setAttribute("_token", request.getSession().getId());
+
+        request.getSession().setAttribute("message_id", m.getId());
+
+
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+}
